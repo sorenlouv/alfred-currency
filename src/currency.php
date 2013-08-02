bead0cebd222609d3ca315a5f1d213345bdcba95
Søren Louv-Jansen <sorenlouv@gmail.com>
2013-08-02 19:59:55 +0200

    added shorthand methods

--- src/currency.php
+++ src/currency.php
@@ -13,7 +13,7 @@ function getRow($currency_from, $currency_to, $amount){
   $data = json_decode($data_json);
 
   // Error occurec. Probabl invalid currency
-  if($data->err){
+  if(isset($data->err)){
     $subtext = "Supported currencies are: DKK, EUR, USD, SEK, GBP, CHF, AUD, NOK";
     $wf->result(time(), $data->err, "Invalid currency", $subtext, 'icon.png');
 
@@ -23,7 +23,7 @@ function getRow($currency_from, $currency_to, $amount){
 
     $clipboard = round($exchange_rate * $amount, 1);
     $text = round($exchange_rate * $amount, 1) . " " . $currency_to;
-    $sub_text = strtoupper($currency_from) . ' to ' . strtoupper($currency_to);
+    $sub_text = $currency_from . ' to ' . $currency_to;
 
     $wf->result(time(), $clipboard, $text, $sub_text, 'icon.png');
   }
@@ -38,18 +38,29 @@ function getResult($query){
   global $wf;
 
   $default_currency = "DKK";
-  //$currencies = array("DKK", "EUR", "USD", "SEK", "GBP", "CHF", "AUD", "NOK");
 
-  $query = trim($query);
-  // $query = trim("40USD to SEK");
+  // Replace symbols
+  $query = str_replace(
+    array("€", "$", "£"),
+    array("EUR", "USD", "GBP"),
+    $query
+  );
+
+  // strip " to "
+  $query = str_replace(" to ", " ", $query);
+
+  // Uppercase and trim
+  $query = strtoupper(trim($query));
 
   // parse query
-  preg_match("/^(\d+)([a-zA-Z]{3})(\sto\s([a-zA-Z]{3}))?$/", $query, $matches);
+  preg_match("/^(\d+)([A-Z]{3})\s?([A-Z]{3})?$/", $query, $matches);
+
+  // print_r($matches);
   if(!empty($matches)){
 
     $amount = $matches[1];
     $currency_from = $matches[2];
-    $currency_to = isset($matches[4]) ? $matches[4] : $default_currency;
+    $currency_to = isset($matches[3]) ? $matches[3] : $default_currency;
 
     // Initial row
     getRow($currency_from, $currency_to, $amount);
@@ -61,4 +72,4 @@ function getResult($query){
   return $wf->toxml();
 }
 
-// echo getResult("40usd to asa");
+// echo getResult("40usd $");
